8531cc3237eb2c1c0c8ee5f3c5a2139cfe5002c4
Taru Karttunen <taruti@taruti.net>
2013-06-12 22:01:42 +0300

    doc.go

---
+++ doc.go
@@ -0,0 +1,30 @@
+/* Automatically detect text language and character set.
+
+Currently supported languages:
+Armenian(hy),
+Chinese(zh),
+Czech(cs),
+Danish(da),
+Dutch/Flemish(nl),
+English(en),
+Estonian(et),
+Finnish(fi),
+French(fr),
+German(de),
+Hungarian(hu),
+Italian(it),
+Japanese(ja),
+Korean(ko),
+Lithuanian(lt),
+Polish(pl),
+Portuguese(pt),
+Romanian(ro),
+Slovak(sk),
+Slovene(sl),
+Spanish(es),
+Swedish(sv) and
+Thai(th).
+
+Adding support for new languages easy, drop a line if you need something.
+*/
+package langdetect
